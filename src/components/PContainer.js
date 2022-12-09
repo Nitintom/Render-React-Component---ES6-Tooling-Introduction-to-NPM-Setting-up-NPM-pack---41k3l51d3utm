4c8820184fb105fbfc10a82f47797afaa29767ef
Nitintom <114360708+Nitintom@users.noreply.github.com>
2022-12-09 22:30:00 +0530

    codeNew

---
+++ src/components/PContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PContainer() {
+  return (
+    <div>
+      <p>I am learning React. My life is getting better.</p>
+    </div>
+  )
+}
+
+export default PContainer
